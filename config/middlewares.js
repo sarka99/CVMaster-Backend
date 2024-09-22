26157bbe5cc7fe0e473e477ae946ef5f703853a8
sarka99 <119513443+sarka99@users.noreply.github.com>
2024-09-22 10:00:14 +0200

    Explicitally allowed access from the frontend to resolve CORS issue, changes to the middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,29 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://cv-master-teal.vercel.app'], 
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
